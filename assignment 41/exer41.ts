ff6071ad6a7e98ca734c4297bb61c0a759b2998a
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2023-11-06 15:44:11 +0500

    uncomment code

--- assignment 41/exer41.ts
+++ assignment 41/exer41.ts
@@ -1,8 +1,8 @@
-// function show_magicians(magicians:string[]){
-//     for(let magician of magicians){
-//         console.log(magician)
-//     }
-//     return magicians
-// }
-// let magicians=["Atif Aslam","Ali Zafar","Asim Azhar","Amna Baig"]
-// show_magicians(magicians)
+function show_magicians(magicians:string[]){
+    for(let magician of magicians){
+        console.log(magician)
+    }
+    return magicians
+}
+let magicians=["Atif Aslam","Ali Zafar","Asim Azhar","Amna Baig"]
+show_magicians(magicians)
